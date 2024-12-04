f44e41334849098f33f6a72a81dc6b9d235a34aa
Mujahid Hussain <mhussain@outlook.com>
2024-12-04 18:41:20 +0000

    Added comments to the code

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ response = requests.get("https:/api.github.com/users/github")
 
 # Check if the request was successful
 
-
 if response.statuys_code == 200:
 	data = response.json() 
-
 print(f"Username: {data['login']}")
 
