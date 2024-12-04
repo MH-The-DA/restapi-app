d4e45a494d223abf9e5a547a1749eae44e7ef1e2
MH-The-DA <157540513+MH-The-DA@users.noreply.github.com>
2024-12-04 15:30:41 -0500

    Update app.py
    Testing Pull

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 response = requests.get("https:/api.github.com/users/github")
 
 # Check if the request was successful
-
+#testing pull
 if response.statuys_code == 200:
 	data = response.json() 
 print(f"Username: {data['login']}")
